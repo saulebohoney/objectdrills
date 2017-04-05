f1bee4f689b3db0ab1911cc5010dab22279395eb
Saule BN <beyonce@thinkful.com>
2017-04-05 11:56:59 -0400

    objectdrills

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+function createMyObject() {
+	let createMyObject = {
+		foo: "bar",
+		answerToUniverse: 42,
+		"olly olly": "oxen free",
+		
+		sayHello: function(){
+			return ('hello');
+		}
+	};
+
+	return createMyObject;	
+}
+
+createMyObject();
