26669f51a14569da9ebe0eea6b9939de1d0d35a1
Saule BN <beyonce@thinkful.com>
2017-04-05 12:31:12 -0400

    addedselfref

--- app.js
+++ app.js
@@ -26,3 +26,19 @@ function updateObject(obj) {
 	obj.bang = "bang";
   return obj;
 } 
+
+//Self reference with THIS
+
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: function(){
+      return (this.firstName + ' ' + this.lastName);
+    }
+  }
+  return person;
+}
+
